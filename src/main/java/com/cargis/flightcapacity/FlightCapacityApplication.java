32af420957b57b80246ded431d07b8d74530c9d5
Kerim Can Macit <kerim.macit@yahoo.com>
2020-03-22 14:54:11 +0300

    Initial commit

---
+++ src/main/java/com/cargis/flightcapacity/FlightCapacityApplication.java
@@ -0,0 +1,13 @@
+package com.cargis.flightcapacity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCapacityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightCapacityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cargis/flightcapacity/FlightCapacityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cargis.flightcapacity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class FlightCapacityApplicationTests {
+
+    @Test
+    public void emptyTest() {
+        Assert.assertTrue(Boolean.TRUE);
+    }
+
+}
