c801f1f3ca28db63c2a5c8636d6c14e3939d001a
Erham Mergen <erham.mergen@n11.com>
2020-04-14 20:49:17 +0300

    additional repository method added

--- src/main/java/com/cargis/flightcapacity/repository/FlightNumberRepository.java
+++ src/main/java/com/cargis/flightcapacity/repository/FlightNumberRepository.java
@@ -4,6 +4,9 @@ import com.cargis.flightcapacity.model.FlightNumber;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FlightNumberRepository extends CrudRepository<FlightNumber, Long> {
+    Optional<FlightNumber> findOneByNumber(String number);
 }
